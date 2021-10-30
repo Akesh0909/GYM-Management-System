c0d2ab8dc4b2fd608a8843c3e80b70d33418f334
Akesh <akeshrao0909a.r@gmail.com>
2021-10-30 15:02:42 +0530

    Enter

--- src/app.js
+++ src/app.js
@@ -111,5 +111,5 @@ app.get("*", (req,res) => {
 })
 
 app.listen(port, () => {
-    console.log(`Server is running at port number ${port}`);
+    console.log(` Server is running at http://localhost:8000/ `);
 });
